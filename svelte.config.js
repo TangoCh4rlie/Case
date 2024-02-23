d9567ddcad256191b130dbc75ee489a5c353f65f
TangoCharlie <elouanreymond@tutanota.com>
2024-02-23 14:49:16 +0100

    CORS problem

--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,21 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+	async headers() {
+		return [
+		  {
+			// matching all API routes
+			source: "/api/:path*",
+			headers: [
+			  { key: "Access-Control-Allow-Credentials", value: "true" },
+			  { key: "Access-Control-Allow-Origin", value: "*" },
+			  { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+			  { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+			]
+		  }
+		]
+	  }
 };
 
 export default config;
