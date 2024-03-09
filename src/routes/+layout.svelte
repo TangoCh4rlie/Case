ebf2697349c8344f2f5f63104ce21eff45895725
TangoCharlie <elouanreymond@tutanota.com>
2024-03-09 18:36:45 +0100

    WIP

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,21 @@
 <script>
 	import '../app.pcss';
+	import { SignIn, SignOut } from '@auth/sveltekit/components';
 	import src from '$lib/images/LogoMyCasesNoBG.png'
 </script>
 
-<header>
-	<img 
-		{src} 
-		alt="Logo de l'application MyCase"
-		class="w-10 m-3"
-	>
+<header class="flex flex-row">
+	<a href="/" class="flex flex-row">
+		<img 
+			{src} 
+			alt="Logo de l'application MyCase"
+			class="w-10 m-3"
+		>
+		<h1>My Cases</h1>
+	</a>
+	<SignOut>
+		<div slot="submitButton" class="buttonPrimary">Sign out</div>
+	</SignOut>
 </header>
 
 <slot />
--- src/routes/profile/+layout.server.ts
+++ src/routes/profile/+layout.server.ts
@@ -4,7 +4,7 @@ export const load = async (event) => {
     const session = await event.locals.auth();
 
     if (!session) {
-        redirect(307, 'test');
+        redirect(307, 'login');
     }
 
     return {
